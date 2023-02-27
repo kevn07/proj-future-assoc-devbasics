03c02e84f547f1159f2663d3955f099725dea163
kevin he <decade901@gmail.com>
2023-02-27 15:48:50 +1100

    chore: updated heading but cooler

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <RuhaabSheikh />
         <NauvooPerez />
         <OllyWilson />
-        <h1>hello world</h1>
+        <h1>hello worrrld</h1>
       </header>
     </div>
   );
